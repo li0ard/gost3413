b41374fd640df03ae47f35b26dacf3aa64d0f6db
Lizard <li0ard@proton.me>
2025-07-07 01:02:22 +0600

    feat: v0.1.0 | Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+    <b>@li0ard/gost3413</b><br>
+    <b>Cipher modes and padding's according to GOST R 34.13-2015 in pure TypeScript</b>
+    <br>
+    <a href="https://li0ard.is-cool.dev/gost3413">docs</a>
+    <br><br>
+    <a href="https://github.com/li0ard/gost3413/blob/main/LICENSE"><img src="https://img.shields.io/github/license/li0ard/gost3413" /></a>
+    <br>
+    <a href="https://npmjs.com/package/@li0ard/gost3413"><img src="https://img.shields.io/npm/v/@li0ard/gost3413" /></a>
+    <a href="https://jsr.io/@li0ard/gost3413"><img src="https://jsr.io/badges/@li0ard/gost3413" /></a>
+    <br>
+    <hr>
+</p>
+
+> [!WARNING]
+> This module contains only wrappers for encryption modes without reference to a specific cipher
+
+## Installation
+
+```bash
+# from NPM
+npm i @li0ard/gost3413
+
+# from JSR
+bunx jsr i @li0ard/gost3413
+```
---
+++ src/index.ts
@@ -0,0 +1,331 @@
+import { bytesToNumberBE, type CipherFunc, concatBytes, numberToBytesBE, xor } from "./utils";
+
+export { type CipherFunc } from "./utils"
+
+/**
+ * Calculate length of padding bytes needed for specific block size.
+ * 
+ * @param dataLength Length of input data
+ * @param blockSize Target block size
+ */
+export const getPadLength = (dataLength: number, blockSize: number): number => {
+    if(dataLength < blockSize) {
+        return blockSize - dataLength;
+    }
+    if(dataLength % blockSize == 0) {
+        return 0;
+    }
+    return blockSize - dataLength % blockSize;
+}
+
+/**
+ * Procedure 1 (aka `Процедура 1`)
+ * 
+ * Just fill in with zeros if necessary
+ * @param data Input data
+ * @param blockSize Target block size
+ */
+export const pad1 = (data: Uint8Array, blockSize: number): Uint8Array => {
+    const padded = new Uint8Array(data.length + getPadLength(data.length, blockSize));
+    padded.set(data);
+    return padded;
+}
+
+/**
+ * Prodecure 2 (aka `Процедура 2` aka `ISO/IEC 7816-4`)
+ * @param data Input data
+ * @param blockSize Target block size
+ */
+export const pad2 = (data: Uint8Array, blockSize: number): Uint8Array => {
+    const padded = new Uint8Array(data.length + 1 + getPadLength(data.length + 1, blockSize));
+    padded.set(data, 0);
+    padded[data.length] = 0x80;
+    return padded;
+}
+
+/**
+ * Unpadding for Procedure 2
+ * @param data Input data
+ * @param blockSize Target block size
+ */
+export const unpad2 = (data: Uint8Array, blockSize: number): Uint8Array => {
+    const lastBlock = data.subarray(data.length - blockSize);
+    let padIndex = -1;
+
+    for (let i = lastBlock.length - 1; i >= 0; i--) {
+        if (lastBlock[i] === 0x80) {
+            padIndex = i;
+            break;
+        }
+    }
+
+    if (padIndex === -1) {
+        throw new Error("Padding marker (0x80) not found");
+    }
+
+    for (let i = padIndex + 1; i < lastBlock.length; i++) {
+        if (lastBlock[i] !== 0) {
+            throw new Error("Invalid padding: non-zero bytes after 0x80");
+        }
+    }
+
+    return data.subarray(0, data.length - (blockSize - padIndex));
+}
+
+/**
+ * Prodecure 3 (aka `Процедура 3`)
+ * 
+ * If length of data matches block size, do nothing; otherwise, use Procedure 2 (`pad2`)
+ * @param data Input data
+ * @param blockSize Block size
+ */
+export const pad3 = (data: Uint8Array, blockSize: number): Uint8Array => {
+    if(getPadLength(data.length, blockSize) == 0) return data;
+    return pad2(data, blockSize);
+}
+
+/**
+ * Wrapper for Electronic Codebook (ECB) mode
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ */
+export const ecb_encrypt = (encrypter: CipherFunc, blockSize: number, data: Uint8Array): Uint8Array => {
+    if (data.length == 0 || data.length % blockSize !== 0) throw new Error("Data not aligned");
+
+    const result = new Uint8Array(data.length);
+    let offset = 0;
+
+    for (let i = 0; i < data.length; i += blockSize) {
+        const chunk = data.slice(i, i + blockSize)
+        const encrypted = encrypter(chunk);
+        result.set(encrypted, offset);
+        offset += encrypted.length;
+    }
+
+    return result.slice();
+}
+
+/**
+ * Wrapper for Electronic Codebook (ECB) mode
+ * @param decrypter Decrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ */
+export const ecb_decrypt = (decrypter: CipherFunc, blockSize: number, data: Uint8Array): Uint8Array => {
+    if (data.length == 0 || data.length % blockSize !== 0) throw new Error("Data not aligned");
+
+    const result = new Uint8Array(data.length);
+    let offset = 0;
+
+    for (let i = 0; i < data.length; i += blockSize) {
+        const chunk = data.slice(i, i + blockSize)
+        const encrypted = decrypter(chunk);
+        result.set(encrypted, offset);
+        offset += encrypted.length;
+    }
+
+    return result.slice();
+}
+
+/**
+ * Wrapper for Output Feedback (OFB) mode
+ * 
+ * For decryption you SHOULD use this function again
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector
+ */
+export const ofb = (encrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    if (iv.length == 0 || iv.length % blockSize !== 0) throw new Error("Invalid IV size");
+
+    let r: Uint8Array[] = [];
+    for (let i = 0; i < iv.length; i += blockSize) {
+        r.push(iv.subarray(i, i + blockSize));
+    }
+
+    const result: Uint8Array[] = [];
+    for(let i = 0; i < (data.length + getPadLength(data.length, blockSize)); i += blockSize) {
+        r = r.slice(1).concat([encrypter(r[0])]);
+        const keystreamBlock = r[r.length - 1];
+        const dataBlock = data.subarray(i, i + blockSize);
+        result.push(xor(keystreamBlock, dataBlock))
+    }
+
+    return concatBytes(...result).slice()
+}
+
+/**
+ * Wrapper for Cipher Block Chaining (CBC) mode
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector
+ */
+export const cbc_encrypt = (encrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    if (data.length == 0 || data.length % blockSize !== 0) throw new Error("Data not aligned");
+    if (iv.length == 0 || iv.length % blockSize !== 0) throw new Error("Invalid IV size");
+
+    let r: Uint8Array[] = [];
+    for (let i = 0; i < iv.length; i += blockSize) {
+        r.push(iv.subarray(i, i + blockSize));
+    }
+
+    const result: Uint8Array[] = [];
+    for(let i = 0; i < data.length; i += blockSize) {
+        result.push(encrypter(xor(r[0], data.subarray(i, i + blockSize))))
+        r = r.slice(1).concat([result[result.length-1]]);
+    }
+
+    return concatBytes(...result).slice()
+}
+
+/**
+ * Wrapper for Cipher Block Chaining (CBC) mode
+ * @param decrypter Decrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector
+ */
+export const cbc_decrypt = (decrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    if (data.length == 0 || data.length % blockSize !== 0) throw new Error("Data not aligned");
+    if (iv.length == 0 || iv.length % blockSize !== 0) throw new Error("Invalid IV size");
+
+    let r: Uint8Array[] = [];
+    for (let i = 0; i < iv.length; i += blockSize) {
+        r.push(iv.subarray(i, i + blockSize));
+    }
+
+    const result: Uint8Array[] = [];
+    for(let i = 0; i < data.length; i += blockSize) {
+        let blk = data.subarray(i, i+blockSize)
+        result.push(xor(r[0], decrypter(blk)))
+        r = r.slice(1).concat([blk])
+    }
+
+    return concatBytes(...result).slice()
+}
+
+/**
+ * Wrapper for Cipher Feedback (CFB) mode
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector
+ */
+export const cfb_encrypt = (encrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    if (iv.length == 0 || iv.length % blockSize !== 0) throw new Error("Invalid IV size");
+
+    let r: Uint8Array[] = [];
+    for (let i = 0; i < iv.length; i += blockSize) {
+        r.push(iv.subarray(i, i + blockSize));
+    }
+
+    const result: Uint8Array[] = [];
+    for(let i = 0; i < (data.length + getPadLength(data.length, blockSize)); i += blockSize) {
+        result.push(xor(encrypter(r[0]), data.subarray(i, i + blockSize)))
+        r = r.slice(1).concat([result[result.length - 1]])
+    }
+
+    return concatBytes(...result).slice()
+}
+
+/**
+ * Wrapper for Cipher Feedback (CFB) mode
+ * @param decrypter Decrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector
+ */
+export const cfb_decrypt = (decrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    if (iv.length == 0 || iv.length % blockSize !== 0) throw new Error("Invalid IV size");
+
+    let r: Uint8Array[] = [];
+    for (let i = 0; i < iv.length; i += blockSize) {
+        r.push(iv.subarray(i, i + blockSize));
+    }
+
+    const result: Uint8Array[] = [];
+    for(let i = 0; i < (data.length + getPadLength(data.length, blockSize)); i += blockSize) {
+        let blk = data.subarray(i, i + blockSize)
+        result.push(xor(decrypter(r[0]), blk))
+        r = r.slice(1).concat([blk])
+    }
+
+    return concatBytes(...result).slice()
+}
+
+/**
+ * Wrapper for counter (CTR) mode
+ * 
+ * For decryption you SHOULD use this function again
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ * @param iv Initialization vector (Half of block size)
+ */
+export const ctr = (encrypter: CipherFunc, blockSize: number, data: Uint8Array, iv: Uint8Array): Uint8Array => {
+    const halfBlockSize = (blockSize / 2) | 0;
+    if (iv.length !== halfBlockSize) throw new Error("Invalid IV size");
+
+    const ctrMax = 1n << (8n * BigInt(halfBlockSize));
+    const maxSize = ctrMax * BigInt(blockSize);
+    if (BigInt(data.length) > maxSize) throw new Error("Too big data");
+
+    const keystreamBlocks: Uint8Array[] = [];
+    for (let ctr = 0; ctr < Math.ceil(data.length / blockSize); ctr++) {
+        keystreamBlocks.push(encrypter(concatBytes(iv, numberToBytesBE(ctr, halfBlockSize))));
+    }
+
+    return xor(concatBytes(...keystreamBlocks), data)
+}
+
+const Rb64 = 0b11011
+const Rb128 = 0b10000111
+
+const macShift = (blockSize: number, data: Uint8Array, xorLsb: number = 0): Uint8Array => {
+    const num = (bytesToNumberBE(data) * BigInt(2)) ^ BigInt(xorLsb);
+    return numberToBytesBE(num, blockSize).slice(-blockSize);
+}
+
+const macKs = (encrypter: (block: Uint8Array) => Uint8Array, blockSize: number): Uint8Array[] => {
+    const Rb = blockSize === 16 ? Rb128 : Rb64;
+    const l = encrypter(new Uint8Array(blockSize));
+    let k1: Uint8Array;
+    if ((l[0] & 0x80) !== 0) {
+        k1 = macShift(blockSize, l, Rb);
+    } else {
+        k1 = macShift(blockSize, l);
+    }
+    let k2: Uint8Array;
+    if ((k1[0] & 0x80) !== 0) {
+        k2 = macShift(blockSize, k1, Rb);
+    } else {
+        k2 = macShift(blockSize, k1);
+    }
+    return [k1, k2];
+}
+
+/**
+ * Wrapper for MAC (CMAC/OMAC1) mode
+ * @param encrypter Encrypting function, that takes block as input
+ * @param blockSize Cipher block size
+ * @param data Input data
+ */
+export const mac = (encrypter: CipherFunc, blockSize: number, data: Uint8Array): Uint8Array => {
+    const [k1, k2] = macKs(encrypter, blockSize);
+    let tailOffset: number;
+    if (data.length % blockSize === 0) {
+        tailOffset = data.length - blockSize;
+    } else {
+        tailOffset = data.length - (data.length % blockSize);
+    }
+    let prev: Uint8Array = new Uint8Array(blockSize);
+    for (let i = 0; i < tailOffset; i += blockSize) {
+        prev = encrypter(xor(data.subarray(i, i + blockSize), prev));
+    }
+    const tail = data.subarray(tailOffset);
+    const xorWithPrev = xor(pad3(tail, blockSize), prev);
+    return encrypter(xor(xorWithPrev, (tail.length === blockSize ? k1 : k2)));
+}
